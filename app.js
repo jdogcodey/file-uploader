7e7cd0748e1004c48d1b6743aa03a514930520b6
jdogcodey <jonnydawson@hotmail.com>
2025-01-29 21:27:55 +0000

    Add log in page on index page

--- app.js
+++ app.js
@@ -15,6 +15,7 @@ app.set("view engine", "ejs");
 
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
+app.use(express.static(path.join(__dirname, "public")));
 
 app.use(
   expressSession({
